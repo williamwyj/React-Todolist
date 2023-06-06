f57ce5d41ad104841e8a4dee62541d5b6734e8a2
William Wang <williamwyj1@gmail.com>
2023-06-05 22:27:59 -0700

    implement cancel button for to do edit and update styling

--- src/components/Todoedit.js
+++ src/components/Todoedit.js
@@ -13,12 +13,27 @@ export default function Todoedit(props) {
     props.updateEditState({ edit: false });
   }
   return (
-    <div className={`module-toDo_item ${props.backgroundColor}`}>
+    <div
+      className={`module-toDo_item module-toDo_edit ${props.backgroundColor}`}
+    >
       <form onSubmit={handleEditSubmit}>
         <label>
           <input name='newTodo' defaultValue='Enter your To do' />
         </label>
-        <button type='submit'>Submit</button>
+        {/* <button type='submit'>Submit</button> */}
+        <button type='submit' className='submit_button'>
+          <FontAwesomeIcon
+            icon={icon({ name: "square-check", style: "regular" })}
+            className='click_icon'
+          />
+        </button>
+        <FontAwesomeIcon
+          icon={icon({ name: "rectangle-xmark", style: "regular" })}
+          className='click_icon'
+          onClick={(e) => {
+            props.updateEditState({ edit: false });
+          }}
+        />
       </form>
     </div>
   );
